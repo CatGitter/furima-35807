d9ae6e37e28d5b48ad6c8b6b4e1e4f7590e60f6a
CatGitter <utautaiiinko@i.softbank.jp>
2021-06-25 19:10:10 +0900

    商品出品ページ表示・データ保存

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,24 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :burden
+  belongs_to :prefecture
+  belongs_to :delivery_day
 
   belongs_to :user
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :image
+    validates :title
+    validates :description
+    validates_inclusion_of :price, in:300..9999999
+  end
+  validates :category_id,     numericality: { other_than: 1 }
+  validates :status_id,       numericality: { other_than: 1 }
+  validates :burden_id,       numericality: { other_than: 1 }
+  validates :prefecture_id,   numericality: { other_than: 1 }
+  validates :delivery_day_id, numericality: { other_than: 1 }
 
 end
