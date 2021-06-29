1b604a8a10bbd311bfa9ab4c4666a1e1dc480084
CatGitter <utautaiiinko@i.softbank.jp>
2021-06-29 15:29:47 +0900

    商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :in_item, only: [:show, :edit, :update]
   before_action :move_to_index, only: [:edit, :update]
 
@@ -33,6 +33,13 @@ class ItemsController < ApplicationController
       render :edit
     end
   end
+
+  def destroy
+    item = Item.find(params[:id])
+      if item.destroy
+        redirect_to root_path
+      end
+    end
 
   private
 
